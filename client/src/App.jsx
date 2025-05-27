56a1a60ae11d3e49b84fac947e1ed8d2e4fd6e67
Rahul <146550441+vrahul15@users.noreply.github.com>
2025-05-28 00:13:43 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
 function App() {
-  return <AppRoutes />;
+ return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
