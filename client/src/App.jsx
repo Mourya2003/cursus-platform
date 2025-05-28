b46199715530449b6571119e260207caf373b77f
Rahul <146550441+vrahul15@users.noreply.github.com>
2025-05-28 11:10:15 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
-import { BrowserRouter } from "react-router-dom";
+
 import './index.css'
 function App() {
  return (
-    <BrowserRouter>
+    
       <AppRoutes />
-    </BrowserRouter>
+  
   );
 }
 
